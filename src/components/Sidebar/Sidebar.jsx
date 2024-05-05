6394665caaac93582d9b03ae12462946c6ef1b6a
Jackie Jr <jwysinger@gmail.com>
2024-05-05 11:28:33 -0500

    jsx layout for top of sidebar complete

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import './Sidebar.css'
+import {assets} from '../../assets/assets'
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-      
+      <div className='top'>
+        <img className='menu' src={assets.menu_icon}/>
+        <div className='new-chat'>
+          <img src={assets.plus_icon} />
+          <p>New Chat</p>
+        </div>
+        <div className='recent'>
+          <p className='recent-title'>Recent</p>
+          <div className='recent-entry'>
+            <img src={assets.message_icon}/>
+            <p>What is react...</p>
+          </div>
+        </div>
+      </div>
+      <div className='bottom'>
+
+      </div>
     </div>
   )
 }
