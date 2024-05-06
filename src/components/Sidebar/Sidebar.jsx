5bfd7d62609759d231f708c2a5da40eaa8705569
Jackie Jr <jwysinger@gmail.com>
2024-05-05 20:37:46 -0500

    jsx layout for sidebar complete

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -20,7 +20,18 @@ const Sidebar = () => {
         </div>
       </div>
       <div className='bottom'>
-
+        <div className='bottom-item recent-entry'>
+          <img src={assets.question_icon}/>
+          <p>Help</p>
+        </div>
+        <div className='bottom-item recent-entry'>
+          <img src={assets.history_icon}/>
+          <p>Activity</p>
+        </div>
+        <div className='bottom-item recent-entry'>
+          <img src={assets.setting_icon}/>
+          <p>Settings</p>
+        </div>
       </div>
     </div>
   )
