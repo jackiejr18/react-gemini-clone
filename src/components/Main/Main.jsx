3371ec33a208e8ebc1ec9cc68fd8ba0d71b09d8b
Jackie Jr <jwysinger@gmail.com>
2024-05-12 16:06:10 -0500

    text and images added to main container in card div

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,6 +9,30 @@ const Main = () => {
         <p>Gemini</p>
         <img src={assets.user_icon}/>
       </div>
+      <div className='main-container'>
+        <div className='greet'>
+          <p><span>Hello, Dev.</span></p>
+          <p>How can I help you today?</p>
+        </div>
+        <div className='cards'>
+          <div className='card'>
+            <p>Suggest beautiful places to see on an upcoming trip</p>
+            <img src={assets.bulb_icon}/>
+          </div>
+          <div className='card'>
+            <p>Briefly summeraze this concept: urban planning</p>
+            <img src={assets.compass_icon}/>
+          </div>
+          <div className='card'>
+            <p>Brainstorm team bonding activites for our work retreat</p>
+            <img src={assets.message_icon}/>
+          </div>
+          <div className='card'>
+            <p>Improve the readability of the following code</p>
+            <img src={assets.code_icon}/>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
