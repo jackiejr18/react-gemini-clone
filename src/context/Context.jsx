088e12f3339e9224b4338be0e959911b26a2d336
Jackie Jr <jwysinger@gmail.com>
2024-05-13 14:06:10 -0500

    creation of 6 useState variables

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,16 +1,20 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import runChat from "../config/gemini";
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
 
+  const [input, setInput] = useState('');
+  const [recentPrompt, setRecentPrompt] = useState('');
+  const [prevPrompts, setPrevPrompts] = useState([]);
+  const [showResult, setShowResult] = useState(false); 
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState('');
 
   const onSent = async (prompt) => {
     await runChat(prompt)
   }
-
-  onSent('what is react js')
 
 
   const contextValue = {
