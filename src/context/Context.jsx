7848fd0acac494eadc6950a457bc4c0116f9d28c
Jackie Jr <jwysinger@gmail.com>
2024-05-14 11:26:54 -0500

    onSent function created

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -13,7 +13,14 @@ const ContextProvider = (props) => {
   const [resultData, setResultData] = useState('');
 
   const onSent = async (prompt) => {
-    await runChat(input)
+  
+    setResultData('')
+    setLoading(true)
+    setShowResult(true)
+    const response = await runChat(input)
+    setResultData(response)
+    setLoading(false)
+    setInput('')
   }
 
 
