ea1949755c38fc8f8c1442364ee57ac60801f637
Jackie Jr <jwysinger@gmail.com>
2024-05-14 10:20:58 -0500

    arrow image returns information on click

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -13,12 +13,21 @@ const ContextProvider = (props) => {
   const [resultData, setResultData] = useState('');
 
   const onSent = async (prompt) => {
-    await runChat(prompt)
+    await runChat(input)
   }
 
 
   const contextValue = {
-
+    prevPrompts,
+    setPrevPrompts,
+    onSent,
+    setRecentPrompt,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    input,
+    setInput
   }
 
   return (
