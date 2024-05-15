cace9f46baadc8a5347061e951c79434b87941df
Jackie Jr <jwysinger@gmail.com>
2024-05-15 10:03:18 -0500

    show result functions clears main div

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -14,28 +14,38 @@ const Main = () => {
         <img src={assets.user_icon}/>
       </div>
       <div className='main-container'>
-        <div className='greet'>
-          <p><span>Hello, Dev.</span></p>
-          <p>How can I help you today?</p>
-        </div>
-        <div className='cards'>
-          <div className='card'>
-            <p>Suggest beautiful places to see on an upcoming trip</p>
-            <img src={assets.bulb_icon}/>
+
+        {!showResult
+          ?<>
+            <div className='greet'>
+              <p><span>Hello, Dev.</span></p>
+              <p>How can I help you today?</p>
+            </div>
+            <div className='cards'>
+              <div className='card'>
+                <p>Suggest beautiful places to see on an upcoming trip</p>
+                <img src={assets.bulb_icon}/>
+              </div>
+              <div className='card'>
+                <p>Briefly summeraze this concept: urban planning</p>
+                <img src={assets.compass_icon}/>
+              </div>
+              <div className='card'>
+                <p>Brainstorm team bonding activites for our work retreat</p>
+                <img src={assets.message_icon}/>
+              </div>
+              <div className='card'>
+                <p>Improve the readability of the following code</p>
+                <img src={assets.code_icon}/>
+              </div>
+            </div>
+          </>
+          :<div className='result'>
+
           </div>
-          <div className='card'>
-            <p>Briefly summeraze this concept: urban planning</p>
-            <img src={assets.compass_icon}/>
-          </div>
-          <div className='card'>
-            <p>Brainstorm team bonding activites for our work retreat</p>
-            <img src={assets.message_icon}/>
-          </div>
-          <div className='card'>
-            <p>Improve the readability of the following code</p>
-            <img src={assets.code_icon}/>
-          </div>
-        </div>
+        }
+
+        
 
         <div className='main-bottom'>
           <div className='search-box'>
