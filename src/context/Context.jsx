e750c27bd1a1e00d928a68fb4209c85004281c2d
Jackie Jr <jwysinger@gmail.com>
2024-05-17 11:46:04 -0500

    removal of star effect and break at bold

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -34,7 +34,8 @@ const ContextProvider = (props) => {
         newResponse += '<b>' + responseArray[i] + '</b>'; 
       }
     }
-    setResultData(newResponse)
+    let newResponse2 = newResponse.split('*').join('<br>')
+    setResultData(newResponse2)
     setLoading(false)
     setInput('')
   }
